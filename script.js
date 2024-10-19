38f733ecff5634d0ed915690cd706be139182865
raza-t580-wsl <beheshti110100@gmail.com>
2024-10-19 13:19:30 +0500

    Create most of the gameController function's methods and properties

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const game = gameController();
 function createGameBoard(size = 3) {
     const board = [];
 
-    //create board
+    //create board based on size
     
     //getBoard();
 
@@ -18,27 +18,46 @@ function Unit() {
     let value = 0;
 
     // addToken();
+    const addToken = (playerToken) => {value = playerToken};
 
     // getValue();
-    
+    const getValue = () => value;
+
     return {addToken, getValue};
 }
 
 function gameController(playerOneName = "Player One", playerOneMark = "X", playerTwoName = "Player Two", playerTwoMark = "O") {
     // initialize gameBoard
+    // const board = createGameBoard();  //will initialize the gameboard once I complete createGameBoard().
+
     // players objects
+    const players = [
+        {name : playerOneName, marker : playerOneMark}, 
+        {name : playerTwoName, marker : playerTwoMark}
+    ];
 
     // assign a player's turn
-    
+    let activePlayer = players[0];  
+
     // switchTurn();
+    const switchTurn = () => activePlayer = activePlayer == players[0] ? players[1] : players[0];
      
     // getActivePlayer();
+    const getActivePlayer = () => activePlayer;
 
     // renderNewRound();
+    const renderNewRound = () => {
+        // board.renderBoard(); // will uncomment this once board is initialized
+        console.log(`It's ${getActivePlayer().name}'s turn. [${getActivePlayer().marker}]`)
+    };
 
     // playRound() -> placeToken -> change turn -> renderNewRound
+    const playRound = (row, column) => {
+
+    };
 
     // renderNewRound [for the first time]
+    renderNewRound();
 
     return {playRound, getActivePlayer};
 }
